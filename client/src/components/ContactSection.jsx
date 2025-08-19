1e78ecb38c976ac8f597bac69c5a214a2d04b5a2
nelson <chinwubanelson@gmail.com>
2025-08-19 20:49:13 +0100

    edit contact form

--- client/src/components/ContactSection.jsx
+++ client/src/components/ContactSection.jsx
@@ -6,7 +6,7 @@ const ContactSection = () => {
   return (
     <section className="w-full min-h-screen flex py-20 border-b border-gray-300 bg-black relative z-40">
       <Container>
-        <div className="w-full  flex flex-col lg:flex-row items-center ">
+        <div className="w-full h-full lg:h-screen  flex flex-col lg:flex-row items-center gap-4 ">
           <div className="h-full flex-1  pt-10 w-full">
             <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-white font-semibold">
               Get in touch:
