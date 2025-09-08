9147bb9d88bb891d4bcee27d1d5232f293b9fa2d
nelson <chinwubanelson@gmail.com>
2025-09-08 21:39:22 +0100

    edit layout

--- client/src/components/ServiceEngineering.jsx
+++ client/src/components/ServiceEngineering.jsx
@@ -7,8 +7,8 @@ const ServiceEngineering = () => {
   return (
     <div className="w-full flex-col h-full flex justify-center items-center mt-20 ">
       <div className=" text-xl md:text-2xl  lg:text-6xl text-white py-8">
-        <h1 className="text-center">We build solutions and grow</h1>
-        <h1 className="text-center">partnerships</h1>
+        <h1 className="text-center">Discover how we engineer </h1>
+        <h1 className="text-center">technological wonders</h1>
       </div>
       <div className="h-full pt-5 w-full flex flex-col  items-center justify-start lg:grid lg:grid-cols-3">
         {cardData.map((card) => (
