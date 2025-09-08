4f264a05a29016aa02119082f55ede9a4426c272
nelson <chinwubanelson@gmail.com>
2025-09-08 06:27:09 +0100

    edit conatiner

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children }) => {
   return (
-    <div className="px-10 w-full h-full  flex justify-center items-center  overflow-x-hidden">
+    <div className="px-3 lg:px-10 w-full h-full  flex justify-center items-center  overflow-x-hidden">
       {children}
     </div>
   );
